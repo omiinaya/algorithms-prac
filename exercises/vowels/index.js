c861ca9183748cc6118adea2c220e28494333dce
Omar Minaya <omiinaya@gmail.com>
2022-06-16 18:08:09 -0400

    vowels.

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,14 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let chars = str.split('')
+    let x = vowels.filter(item => chars.includes(item))
+    console.log(x.length)
+    return x.length
+}
+
+vowels("hi there!")
 
 module.exports = vowels;
