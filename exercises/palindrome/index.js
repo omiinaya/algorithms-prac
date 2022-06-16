1facea2f102f7f8619ab4d8b5b3e0f1f0fd0d7d6
Omar Minaya <omiinaya@gmail.com>
2022-06-16 17:44:32 -0400

    .

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,10 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    const reverse = str.split('').reverse().join('')
+    if (str != reverse) return false
+    return true
+}
 
 module.exports = palindrome;
