94e11dc6451b4239c025318a2d64fedef1167b95
Omar Minaya <omiinaya@gmail.com>
2022-06-17 01:06:45 -0400

    .

--- exercises/testing.js
+++ exercises/testing.js
@@ -13,6 +13,11 @@ function capitalize(str) {
     return x.join(' ')
 }
 
+function reverseInt(n) {
+    const reversed = n.toString().split('').reverse().join('')
+    return parseInt(reversed) * Math.sign(n)
+}
+
 function chunk(array, size) {
     var chunks = []
     for (var i = 0; i < array.length; i = i + size) {
@@ -20,6 +25,15 @@ function chunk(array, size) {
         chunks.push(chunk)
     }
     return chunks
+}
+
+function steps(n) {
+    for (var i = 1; i <= n; i++) {
+        var x = '#'
+        var y = x.repeat(i)
+        var z = y.padEnd(n, " ")
+        console.log(z)
+    }
 }
 
 function fizzBuzz(n) {
@@ -31,4 +45,44 @@ function fizzBuzz(n) {
         else if (buzz) console.log('buzz')
         else console.log(i)
     }
+}
+
+function reverse(str) {
+    return str.split('').reverse().join('')
+}
+
+function vowels(str) {
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let chars = str.split('')
+    let x = vowels.filter(item => chars.includes(item))
+    console.log(x.length)
+    return x.length
+}
+
+function pyramid(n) {
+    for (let i = 1; i <= n; i++) {
+        let x = ' '.repeat(n - i);          //total spaces - current amount of #
+        let y = '#'.repeat(i * 2 - 1)       //amount of blocks necessary for each step
+        console.log(x + y + x);
+    }
+}
+
+function maxChar(str) {
+    let charMap = {}
+    let max = 0
+    let maxChar = ''
+
+    str.split('').forEach(char => {
+        charMap[char] = charMap[char] + 1 || 1;
+    })
+
+    for (let char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char] //increment max when new max found
+            maxChar = char //update maxChar when new one found
+        }
+    }
+
+    console.log(maxChar)
+    return maxChar
 }
